05b7721a058e0304e885f3e16bee48ba7122d606
yusufuysal <yusuf.uysal@agu.edu.tr>
2022-03-14 00:03:36 +0300

    Add title and filter buttons

--- menu/src/App.js
+++ menu/src/App.js
@@ -1,10 +1,36 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
 
 function App() {
-  return <h2>menu project setup</h2>;
+  // const [category, setCategory] = useState([])
+
+  let uniqueCategories = new Set();
+  uniqueCategories.add("all");
+  items.forEach((item) => {
+    uniqueCategories.add(item.category);
+  });
+  const uniqueCategoriesArray = Array.from(uniqueCategories);
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <div className="btn-container">
+          {/*   find the number of distinct categories in the data array and make a new set of those categories     */}
+          {/*   map through the categories array and render the Categories component for each category     */}
+          {uniqueCategoriesArray.map((category) => {
+            return <Categories category={category} />;
+          })}
+          {}
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- menu/src/Categories.js
+++ menu/src/Categories.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = (props) => {
+  return <button className="filter-btn">{props.category}</button>;
 };
 
 export default Categories;
