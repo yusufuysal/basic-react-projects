b9ae85910e87ce4bfd6d67f0e5e80e62b3cc7cfd
yusufuysal <yusuf.uysal@agu.edu.tr>
2022-03-13 03:19:53 +0300

    Complete the accordion project

--- accordion/src/App.js
+++ accordion/src/App.js
@@ -1,8 +1,19 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
 function App() {
-  return <h2>accordion project setup</h2>;
+  return (
+    <main>
+      <div className="container">
+        <h3>Questions And Answers About Login</h3>
+        <section className="info">
+          {data.map((item) => {
+            return <SingleQuestion key={item.id} {...item} />;
+          })}
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
